bd23bf04f85b7ff6b537b3509fbe874b556926b3
swaroopc236 <swaroopc236@gmail.com>
2021-03-14 07:51:34 +0530

    root route, server 💻, auth basic

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+authRoutes(app);
 
 app.get('/', (req, res) => {
-    res.send({
-        msg: "Hello!"
-    })
-})
+	res.send({
+		msg: 'Hello!',
+	});
+});
 
-const PORT = process.env.PORT || 5000
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log('Serving on port 5000...')
-})
+	console.log(`Serving on port ${PORT}...`);
+});
