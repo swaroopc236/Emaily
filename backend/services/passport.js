19afa9bfb5a3faaf053cca80c7e5666ceb7869fa
swaroopc236 <swaroopc236@gmail.com>
2021-03-21 02:02:58 +0530

    Google OAuth

--- backend/services/passport.js
+++ backend/services/passport.js
@@ -1,6 +1,17 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id).then((user) => done(null, user));
+});
 
 passport.use(
 	new GoogleStrategy(
@@ -10,9 +21,15 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 		},
 		(accessToken, refreshToken, profile, done) => {
-			console.log('AccessToken', accessToken);
-			console.log('RefreshToken', refreshToken);
-			console.log('Profile', profile);
+			User.findOne({ googleId: profile.id }).then((existingUser) => {
+				if (existingUser) {
+					done(null, existingUser);
+				} else {
+					new User({ googleId: profile.id })
+						.save()
+						.then((user) => done(null, user));
+				}
+			});
 		}
 	)
 );
